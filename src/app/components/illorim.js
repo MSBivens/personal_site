e68f1bb9f693e023f393293706a65d66799d7242
MSBivens <mikebiv95@gmail.com>
2023-10-04 23:40:01 -0700

    building button function to build content

--- src/app/components/illorim.js
+++ src/app/components/illorim.js
@@ -1,7 +1,47 @@
 import React from "react";
 
-const illorim = () => {
-  return <div>illorim</div>;
+const IllorimComponent = () => {
+  const generateContent = () => {
+    const squaresContent = [
+      {
+        title: "World Anvil",
+        imageUrl: "/world_anvil_image.jpg", // Replace with actual image URL
+        link: "https://www.worldanvil.com/w/illorim-aracthius",
+      },
+      {
+        title: "Vocal",
+        imageUrl: "/vocal_image.jpg", // Replace with actual image URL
+        link: "https://vocal.media/authors/michael-bivens-ii17q0xsr",
+      },
+      {
+        title: "Last of the Mastiff Riders",
+        imageUrl: "/mastiff_riders_image.jpg", // Replace with actual image URL
+        link: "https://mastiffriders.com",
+      },
+    ];
+
+    return squaresContent.map((content, index) => (
+      <a
+        key={index}
+        href={content.link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="square"
+        style={{ backgroundImage: `url(${content.imageUrl})` }}
+      >
+        <div className="square-content">
+          <span>{content.title}</span>
+        </div>
+      </a>
+    ));
+  };
+
+  return (
+    <div>
+      {/* Call generateSquares function to render three squares */}
+      <div className="squares-container">{generateContent()}</div>
+    </div>
+  );
 };
 
-export default illorim;
+export default IllorimComponent;
