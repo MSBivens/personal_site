3c46e5400c0460abfbced09f407096499240c65c
MSBivens <mikebiv95@gmail.com>
2023-10-04 23:54:06 -0700

    updating image src

--- src/app/components/illorim.js
+++ src/app/components/illorim.js
@@ -1,45 +1,56 @@
 import React from "react";
+import Image from "next/image";
+import styles from "../page.module.css";
+
+import meImage from "../../../public/me.jpeg"; // Import the image using file path
+// import vocalImage from "../public/vocal_image.jpg"; // Import another image
+// import mastiffRidersImage from "../public/mastiff_riders_image.jpg"; // Import another image
 
 const IllorimComponent = () => {
-  const generateContent = () => {
-    const squaresContent = [
-      {
-        title: "World Anvil",
-        imageUrl: "/world_anvil_image.jpg", // Replace with actual image URL
-        link: "https://www.worldanvil.com/w/illorim-aracthius",
-      },
-      {
-        title: "Vocal",
-        imageUrl: "/vocal_image.jpg", // Replace with actual image URL
-        link: "https://vocal.media/authors/michael-bivens-ii17q0xsr",
-      },
-      {
-        title: "Last of the Mastiff Riders",
-        imageUrl: "/mastiff_riders_image.jpg", // Replace with actual image URL
-        link: "https://mastiffriders.com",
-      },
-    ];
-
-    return squaresContent.map((content, index) => (
-      <a
-        key={index}
-        href={content.link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="square"
-        style={{ backgroundImage: `url(${content.imageUrl})` }}
-      >
-        <div className="square-content">
-          <span>{content.title}</span>
-        </div>
-      </a>
-    ));
-  };
+  const squaresContent = [
+    {
+      title: "World Anvil",
+      image: meImage,
+      link: "https://www.worldanvil.com/w/illorim-aracthius",
+    },
+    {
+      title: "Vocal",
+      image: meImage,
+      link: "https://vocal.media/authors/michael-bivens-ii17q0xsr",
+    },
+    {
+      title: "Last of the Mastiff Riders",
+      image: meImage,
+      link: "https://mastiffriders.com",
+    },
+  ];
 
   return (
     <div>
-      {/* Call generateSquares function to render three squares */}
-      <div className="squares-container">{generateContent()}</div>
+      {/* Render squares using the imported images */}
+      <div className="squares-container">
+        {squaresContent.map((content, index) => (
+          <a
+            key={index}
+            href={content.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="square"
+          >
+            <div className="square-content">
+              <Image
+                src={content.image}
+                alt={`Image for ${content.title}`}
+                width={200}
+                height={200}
+                className={styles.headshot}
+                priority
+              />
+              <span>{content.title}</span>
+            </div>
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
