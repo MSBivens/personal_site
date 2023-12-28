bb85c0f3b309dbdb28dc38a0ef5e533594217c98
MSBivens <mikebiv95@gmail.com>
2023-12-28 11:21:40 -0800

    adding icons

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,13 @@ import Image from "next/image";
 import styles from "./globals.css";
 // import styles from "./page.module.css";
 
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaMediumM } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { MdOutlinePodcasts } from "react-icons/md";
+
+
 export default function Home() {
 
   return (
@@ -31,10 +38,23 @@ export default function Home() {
                     <div class="text-center text-gray-400 text-xs font-semibold">
                         <p>Software Dev Student</p>
                     </div>
-        
-                    <div class="text-center my-3">
-                        <a class="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">View Profile</a>
-                    </div>
+                    <div class="flex items-center gap-6 my-3">
+                    <a class="icon" href="https://www.linkedin.com/in/msbivens/" target="_blank">
+                      <FaLinkedinIn className="text-2xl" />
+                    </a>
+                    <a class="icon" href="https://twitter.com/MSBivens0x" target="_blank">
+                      <FaXTwitter className="text-2xl"/> 
+                    </a>
+                    <a class="icon" href="https://medium.com/@msbivens0x" target="_blank">
+                      <FaMediumM className="text-2xl"/>
+                    </a>
+                    <a class="icon" href="https://github.com/MSBivens" target="_blank">
+                      <FaGithub className="text-2xl"/>
+                    </a>
+                    <a class="icon" href="https://www.podchaser.com/creators/michael-bivens-107a99qAVv" target="_blank">
+                      <MdOutlinePodcasts className="text-2xl"/>
+                    </a>
+                  </div>
                 </div>
             </div>
         </div>
